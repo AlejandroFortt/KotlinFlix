da01a1dcadf7adb453dbde5d87f9e3f0835e8f77
Alejandro Fort <alejandrofortt@gmail.com>
2020-08-02 21:27:16 -0500

    Agregamos Timber al proyecto.
    Timber es una librería para mejorar los logs en Android.
    See: github.com/JakeWharton/timber

---
+++ app/src/main/java/com/fortatic/apps/kotlinflix/MainApplication.kt
@@ -0,0 +1,25 @@
+package com.fortatic.apps.kotlinflix
+
+import android.app.Application
+import timber.log.Timber
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Evitamos mostrar mensajes en la consola cuando estamos en modo "release"
+        if (BuildConfig.DEBUG){
+            Timber.plant(Tree())
+        }
+    }
+}
+
+/**
+ * Creamos la clase Tree para poder personalizar el tag por defecto que nos
+ * proporciona Timber.
+ */
+class Tree : Timber.DebugTree() {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        super.log(priority, "$tag FATAL", message, t)
+    }
+}
