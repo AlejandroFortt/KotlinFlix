70ee32b7505cfdf3353e6da507f35fda92fe2452
Alejandro Fort <alejandrofortt@gmail.com>
2020-08-06 23:09:20 -0500

    Movie como objeto de dominio manipulable por el app.
    Creamos Movie, el objeto de dominio que será usando por los Adapters,
    ViewModels, Activitys, Fragments, etc.
    También agregamos lógica para decidir qué tamaño de imagen es el
    adecuado para cada dispositivo, optimizando recursos y ancho de banda.

---
+++ app/src/main/java/com/fortatic/apps/kotlinflix/domain/DomainExtensions.kt
@@ -0,0 +1,34 @@
+package com.fortatic.apps.kotlinflix.domain
+
+import android.content.res.Resources
+import android.util.DisplayMetrics
+import com.fortatic.apps.kotlinflix.BuildConfig
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+const val SMALL_SIZE = "w200"
+const val MEDIUM_SIZE = "w300"
+const val LARGE_SIZE = "w400"
+const val XLARGE_SIZE = "w500"
+const val TABLET_SIZE = 7
+
+fun getImageBaseUrl(): String {
+    val url = BuildConfig.IMAGE_BASE_URL
+    val dm = Resources.getSystem().displayMetrics
+    val density = dm.densityDpi
+    val size = if (isTablet(dm)) XLARGE_SIZE else when (density) {
+        in DisplayMetrics.DENSITY_XXHIGH until DisplayMetrics.DENSITY_XXXHIGH -> XLARGE_SIZE
+        in DisplayMetrics.DENSITY_XHIGH until DisplayMetrics.DENSITY_XXHIGH -> LARGE_SIZE
+        in DisplayMetrics.DENSITY_MEDIUM until DisplayMetrics.DENSITY_XHIGH -> MEDIUM_SIZE
+        in DisplayMetrics.DENSITY_LOW until DisplayMetrics.DENSITY_MEDIUM -> SMALL_SIZE
+        else -> MEDIUM_SIZE
+    }
+    return url.plus(size)
+}
+
+fun isTablet(dm: DisplayMetrics): Boolean {
+    val x = (dm.widthPixels / dm.xdpi.toDouble()).pow(2)
+    val y = (dm.heightPixels / dm.ydpi.toDouble()).pow(2)
+    val inches = sqrt(x.plus(y))
+    return inches > TABLET_SIZE
+}
---
+++ app/src/main/java/com/fortatic/apps/kotlinflix/domain/Movie.kt
@@ -0,0 +1,34 @@
+package com.fortatic.apps.kotlinflix.domain
+
+/**
+ * Los objetos de dominio son data class de Kotlin que representan las cosas
+ * en nuestra aplicación.
+ * Estos son los objetos que deben mostrarse en la pantalla o manipularse por la aplicación.
+ *
+ * @see database para los objetos usados en Room.
+ * @see network para objetos que analizan o preparan llamadas de red.
+ */
+data class Movie(
+    val popularity: Double,
+    val voteCount: Int,
+    val video: Boolean,
+    val posterPath: String,
+    val id: Int,
+    val adult: Boolean,
+    val backdropPath: String,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val genreIds: List<Int>,
+    val title: String,
+    val voteAverage: Double,
+    val overview: String,
+    val releaseDate: String
+) {
+    /**
+     * Url completas, usadas por Glide para cargar los recursos mostrados en la UI.
+     */
+    val posterUrl: String
+        get() = getImageBaseUrl().plus(posterPath)
+    val backdropUrl: String
+        get() = getImageBaseUrl().plus(backdropPath)
+}
