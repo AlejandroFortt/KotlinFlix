f872d6327c22b2947f55cc94eb9635ef73629f9f
Alejandro Fort <alejandrofortt@gmail.com>
2020-08-08 01:29:14 -0500

    [Fix] Se corrigió un tipo de dato guardado en Room.
    Room no admite el almacenamiento de referencias a objetos, en su lugar
    debemos usar convertidores de tipos y almacenar datos primitivos.
    En este commit corregimos el intento de almacenamiento de un List<Int>
    usando convertidores que transforman la lista en un String y viceversa.
    
    También añadimos la dependencia room-ktx para soportar el uso de
    coroutinas en Room.

--- app/src/main/java/com/fortatic/apps/kotlinflix/database/AppDatabase.kt
+++ app/src/main/java/com/fortatic/apps/kotlinflix/database/AppDatabase.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [DatabaseMovie::class],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract val movieDao: MovieDao
 
---
+++ app/src/main/java/com/fortatic/apps/kotlinflix/database/Converters.kt
@@ -0,0 +1,26 @@
+package com.fortatic.apps.kotlinflix.database
+
+import androidx.room.TypeConverter
+
+private const val DELIMITER = ","
+
+/**
+ * Esta clase convierte una lista a datos primitivos, esto debido a que Room no acepta objetos.
+ * @see https://developer.android.com/training/data-storage/room/referencing-data#type-converters
+ */
+class Converters {
+
+    @TypeConverter
+    fun fromString(linearValue: String?): List<Int>? {
+        return linearValue?.let { value ->
+            value.split(DELIMITER).map {
+                it.trim().toInt()
+            }
+        }
+    }
+
+    @TypeConverter
+    fun listToString(list: List<Int>?): String? {
+        return list?.joinToString()
+    }
+}
