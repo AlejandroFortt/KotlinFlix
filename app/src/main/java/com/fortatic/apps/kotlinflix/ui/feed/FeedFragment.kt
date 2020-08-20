cca29928246a273238f04472497a08c813302d40
Alejandro Fort <alejandrofortt@gmail.com>
2020-08-20 17:16:23 -0500

    Consumiendo datos desde FeedFragment.
    Inflamos nuestro fragment en onCreateView pero observamos los cambios de
    "movies" en onViewCreated porque aquí tenemos la certeza de que la vista
    se creó correctamente.

--- app/src/main/java/com/fortatic/apps/kotlinflix/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/fortatic/apps/kotlinflix/ui/feed/FeedFragment.kt
@@ -5,21 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.fortatic.apps.kotlinflix.databinding.FragmentFeedBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FeedFragment : Fragment() {
 
+    private val feedViewModel: FeedViewModel by viewModels()
+    private lateinit var binding: FragmentFeedBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentFeedBinding.inflate(
+        binding = FragmentFeedBinding.inflate(
             inflater, container, false
-        )
+        ).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val movieAdapter = MovieAdapter()
+        binding.rvFeed.adapter = movieAdapter
+
+        feedViewModel.movies.observe(viewLifecycleOwner, { listMovies ->
+            movieAdapter.submitList(listMovies)
+        })
     }
 }
