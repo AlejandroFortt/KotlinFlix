2cd673cfe506cc06a6177521dc945a8399f030c1
Alejandro Fort <alejandrofortt@gmail.com>
2020-08-20 18:24:42 -0500

    [Fix] Soporte fuera de linea.
    Había un error en MovieRepository al intentar traer los datos del
    servidor cuando el dispositivo estaba sin conexión, solucionamos
    dicho error, encerrando la petición al servidor en un bloque try/catch,
    de tal forma que la excepción quede capturada en un lugar seguro.

--- app/src/main/java/com/fortatic/apps/kotlinflix/repository/MovieRepository.kt
+++ app/src/main/java/com/fortatic/apps/kotlinflix/repository/MovieRepository.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
+import timber.log.Timber
 import javax.inject.Inject
 
 /**
@@ -26,8 +27,15 @@ class MovieRepository @Inject constructor(
 
     suspend fun getMoviesFromNetwork() {
         withContext(Dispatchers.IO) {
-            val moviesFromNetwork = Api.retrofitService.getPlaylist()
-            saveDataInDatabase(moviesFromNetwork.asDatabaseModel())
+            val moviesFromNetwork = try {
+                Api.retrofitService.getPlaylist()
+            } catch (e: Exception) {
+                Timber.e(e)
+                null
+            }
+            moviesFromNetwork?.let {
+                saveDataInDatabase(it.asDatabaseModel())
+            }
         }
     }
 
